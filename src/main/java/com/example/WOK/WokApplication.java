0e16081bc69e0b1c4f4ae5f36d7465f0d9463080
Nguyễn Quốc Bảo An <baoandng07@gmail.com>
2024-10-06 15:00:48 +0700

    Create project

---
+++ src/main/java/com/example/WOK/WokApplication.java
@@ -0,0 +1,13 @@
+package com.example.WOK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WOK/WokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WOK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
